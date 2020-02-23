5a8cdf33e7812aa1a2b2ae8c79fa9127c0d04d2b
garciaac01 <garciaac01@gmail.com>
2020-02-23 16:03:27 -0500

    First look at state and click events.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,13 +3,27 @@ import './App.css';
 import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Andy", age: 36 },
+      { name: "Harrison", age: 1 },
+      { name: "Gizmo", age: 9 }
+    ]
+  };
+
+  switchNameHandler = () => {
+    console.log("Was clicked!")
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working.</p>
-        <Person name="Andy" age="36" />
-        <Person name="Harrison" age="1">I'm a baby! :)</Person>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
     // return React.createElement("div", { className: "App" }, React.createElement("h1", null, "Does this work now???"));
