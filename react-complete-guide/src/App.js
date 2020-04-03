acd22f5f1a70f45638aa5c044c8b5ddd56b427e9
garciaac01 <garciaac01@gmail.com>
2020-04-03 07:15:29 -0400

    Example of module-based css, enabled by making an edit to the webpack dev and prod files. This is enabled differently depending on the version of react scripts you're using.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from "./Person/Person";
 
 
@@ -52,21 +52,10 @@ class App extends Component {
   };
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
-    };
-
     // render runs every time React refreshes the page, so we can run some code in here.
     let persons = null;
+    let btnClass = '';
+
     if (this.state.showPersons) {
       persons = (
         < div >
@@ -82,28 +71,24 @@ class App extends Component {
         </div >
       );
 
-      // style.backgroundColor = 'red';
-      // style[':hover'] = {
-      //   backgroundColor: 'salmon',
-      //   color: 'black'
-      // }
+      btnClass = classes.Red;
     }
 
-    const classes = []
+    const assignedClasses = []
     if (this.state.persons.length <= 2) {
-      classes.push('red'); // classes = ['red]
+      assignedClasses.push(classes.red); // classes = ['red]
     }
 
     if (this.state.persons.length <= 1) {
-      classes.push('bold'); // classes = ['red, 'bold]
+      assignedClasses.push(classes.bold); // classes = ['red, 'bold]
     }
 
     return (
-      <div className="App" >
+      <div className={classes.App} >
         <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(' ')}>This is really working.</p>
-        <StyledButton alt={this.state.showPersons}
-          onClick={this.togglePersonsHandler}>Toggle Persons</StyledButton>
+        <p className={assignedClasses.join(' ')}>This is really working.</p>
+        <button className={btnClass}
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>
     );
