2554c0d0ba19e24a0c2874fb4ce98dad671a7f58
garciaac01 <garciaac01@gmail.com>
2020-04-02 22:09:38 -0400

    adding radium; adding hover property to button w/radium

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium from 'radium';
 import Person from "./Person/Person";
 
 class App extends Component {
@@ -57,7 +58,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     // render runs every time React refreshes the page, so we can run some code in here.
@@ -78,6 +83,10 @@ class App extends Component {
       );
 
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
 
     const classes = []
@@ -103,4 +112,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
