30c5b8c84531f8c87927f0dbd1952b845f30eeb6
garciaac01 <garciaac01@gmail.com>
2020-04-13 20:38:38 -0400

    Another way of doing the HOC wrapper

--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import classes from './App.css';
 import Persons from "../components/Persons/Persons";
 import Cockpit from '../components/Cockpit/Cockpit';
-import WithClass from '../hoc/WithClass';
+import withClass from '../hoc/withClass';
 
 class App extends Component {
   constructor(props) {
@@ -98,7 +98,7 @@ class App extends Component {
 
 
     return (
-      <WithClass classes={classes.App} >
+      <Fragment>
         <button onClick={() => {
           this.setState({ showCockpit: false });
         }}
@@ -111,10 +111,10 @@ class App extends Component {
           clicked={this.togglePersonsHandler}
         /> : null}
         {persons}
-      </WithClass>
+      </Fragment>
     );
     // return React.createElement("div", { className: "App" }, React.createElement("h1", null, "Does this work now???"));
   }
 }
 
-export default App;
+export default withClass(App, classes.App);
