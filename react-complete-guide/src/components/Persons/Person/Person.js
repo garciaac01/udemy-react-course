7ede41eafe6e6d55cf94a91f2088e4b779f2c43e
garciaac01 <garciaac01@gmail.com>
2020-04-14 07:44:42 -0400

    Different option of giving focus to the last element in the list.

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -5,8 +5,12 @@ import withClass from '../../../hoc/withClass';
 import classes from "./Person.css";
 
 class Person extends Component {
+    constructor(props) {
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
     componentDidMount() {
-        this.inputElement.focus();
+        this.inputElementRef.current.focus();
     }
 
     render() {
@@ -16,7 +20,7 @@ class Person extends Component {
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input
-                    ref={(inputEl) => { this.inputElement = inputEl }}
+                    ref={this.inputElementRef}
                     type="text"
                     onChange={this.props.changed}
                     value={this.props.name}
